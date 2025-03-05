87ff7ffcccf64501dbb4497569d43fa461acbe65
Robert-t-t <rcdiaz293@gmail.com>
2025-03-04 17:26:34 -0800

    Problem 2

--- CodingPracticeProblemsOverview/CodingPracticeProblemsOverview.java
+++ CodingPracticeProblemsOverview/CodingPracticeProblemsOverview.java
@@ -1,12 +1,20 @@
 package CodingPracticeProblemsOverview;
 
+import java.util.Scanner;
+
 public class CodingPracticeProblemsOverview {
+
+
 
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello word");
+        int userInput = 0;
 
+        userInput = scanner.nextInt();
+
+        System.out.println(userInput*2);
 
     }
 
